4c3f05d175912ffef2d858fb5f63803ca1c41053
yosafatrend <yosafatrendip@gmail.com>
2020-09-07 23:18:55 +0700

    Add project files.

---
+++ KontolApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using HomoForKids.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontolApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var studentRepo = new StudentRepositories();
+            var listStudent = studentRepo.PrintStudents();
+
+            foreach (var student in listStudent)
+            {
+                Console.WriteLine(student.Name + " " +student.Nis + " "+ student.Email);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Repositories/StudentRepositories.cs
@@ -0,0 +1,52 @@
+﻿using HomoForKids.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomoForKids.Repositories
+{
+    public class StudentRepositories
+    {
+        public List<Student> PrintStudents()
+        {
+            var studentList = new List<Student>();
+            studentList.Add(new Student
+            {
+                 Nis = 15236,
+                 Name = "Rendi",
+                 Email = "yosafatrendip@gmail.com"
+            });
+
+            studentList.Add(new Student
+            {
+                Nis = 15237,
+                Name = "Jordi",
+                Email = "jordi@gmail.com"
+            });
+
+            studentList.Add(new Student
+            {
+                Nis = 15238,
+                Name = "Parno",
+                Email = "parno@gmail.com"
+            });
+
+            studentList.Add(new Student
+            {
+                Nis = 15239,
+                Name = "Ratno",
+                Email = "ratno@gmail.com"
+            });
+
+            studentList.Add(new Student
+            {
+                Nis = 15239,
+                Name = "Rani",
+                Email = "rani@gmail.com"
+            });
+            return studentList;
+        }
+    }
+}
